06a202f991401c27daa813c542571a5b9e160e04
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-01-20 01:09:03 +0200

    Moving Enemy

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -1,23 +1,21 @@
 import pygame
+import math
+import os
 
 class Enemy:
-    def __init__(self, x, y, width, height):
-        """
-        :param width: int
-        :param height: int
-        :param x: int
-        :param y: int
-        """
-        self.x = x
-        self.y = y
-        self.width = width
-        self.health = height
-        self.imgs = []
+    def __init__(self):
+        self.width = 20
+        self.health = 20
+        self.imgs = [pygame.image.load(os.path.join("assets", "bl-1-Monster No001-1.png")).convert_alpha()]
         self.img = None
         self.health = 0
         self.animation_count = 0
         self.speed = 1
-        self.path = []
+        self.path = [(21, 460), (314, 462), (371, 447), (398, 403), (407, 295), (430, 246), (478, 223), (522, 230), (564, 247), (591, 287), (598, 338), (625, 371), (666, 395), (718, 396), (770, 406), (802, 430), (851, 458), (899, 469), (958, 470), (998, 471)]
+        self.path_pos = 0
+        self.direction = (0, 0)
+        self.x = self.path[0][0]
+        self.y = self.path[0][1]
 
     def draw(self, window):
         """
@@ -29,7 +27,8 @@ class Enemy:
         self.animation_count += 1
         if self.animation_count >= len(self.imgs):
             self.animation_count = 0
-        window.blit(self.img, (self.x, self.y))
+        window.blit(self.img, (self.x - self.img.get_width()/2, self.y - self.img.get_height()/2 - 35))
+        self.move()
 
     def collide(self, x, y):
         """
@@ -48,7 +47,20 @@ class Enemy:
         Move enemy
         :return: None
         """
-        pass
+
+        x1, y1 = self.path[self.path_pos]
+        if self.path_pos + 1 >= len(self.path):
+            x2, y2 = x1, y1
+        else:
+            x2, y2 = self.path[self.path_pos + 1]
+
+        mag = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        if mag != 0:
+            self.direction = ((x2 - x1) / mag, (y2 - y1) / mag)
+            self.x += self.direction[0] * self.speed
+            self.y += self.direction[1] * self.speed
+            if (self.x, self.y) >= self.path[self.path_pos + 1]:
+                self.path_pos += 1
 
     def hit(self, damage):
         """
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+from enemies.enemy import Enemy
 
 class Game:
     def __init__(self, width, height):
@@ -17,18 +18,31 @@ class Game:
         self.bg = pygame.image.load(os.path.join("assets", "lvl1.jpg"))
         self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
         self.running = False
+        self.clicks = []
 
+        self.gen_enemies()
 
     def run(self):
         self.running = True
         clock = pygame.time.Clock()
+
         while self.running:
             clock.tick(60)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.clicks.append(pos)
+                    print(self.clicks)
+
+            self.draw()
+
         pygame.quit()
+
 
     def draw(self):
         """
@@ -36,4 +50,19 @@ class Game:
         :return: None
         """
         self.window.blit(self.bg, (0, 0))
+        for p in self.clicks:
+            pygame.draw.circle(self.window, (255, 0, 0), (p[0], p[1]), 5, 0)
+
+        for enemy in self.enemies:
+            enemy.draw(self.window)
         pygame.display.update()
+
+    def gen_enemies(self):
+        """
+        Generate enemies
+        :return: None
+        """
+        self.enemies.append(Enemy())
+
+g = Game(1000, 700)
+g.run()
